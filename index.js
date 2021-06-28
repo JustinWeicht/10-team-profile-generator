59e9a5867909576c40fa4ad844cf6c282462f24c
Justin Weicht <justinweicht11@gmail.com>
2021-06-27 21:36:51 -0400

    added constructors class profiles to index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateHTML = require('./src/generateHTML');
 
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
 const teamArray = [];
 
 const managerQs = () => {
