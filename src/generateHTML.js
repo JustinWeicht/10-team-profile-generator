5edd8825e9801d764da6c9261caf2de6f6c52a09
Justin Weicht <justinweicht11@gmail.com>
2021-06-27 22:42:41 -0400

    copied demo.html content into generateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,8 +1,72 @@
-// generate markdown for README using data sent from index.js
-const generateMarkdown = (data) => {
-  return `
-  
-  `;
-}
+// generate content for HTML using data sent from index.js with classes
+const generateHTML = (manager) => {
+    return `
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${manager.name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Manager</h6>            
+            <p class="card-text">ID: ${manager.id}</p>
+            <p class="card-text">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+            <p class="card-text">Office Number: ${manager.officeNumber}</p>
+        </div>
+    </div>
+    `;
+};
+
+const generateHTML = (engineer) => {
+    return `
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${engineer.name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${engineer.role}</h6>     
+            <p class="card-text">ID: ${engineer.id}</p>
+            <p class="card-text">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+            <p>GitHub: <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</a></p>
+        </div>
+    </div>
+    `;
+};
+
+const generateHTML = (intern) => {
+    return `
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${intern.name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${intern.role}</h6>     
+            <p class="card-text">ID: ${intern.id}</p>
+            <p class="card-text">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+            <p class="card-text">School: ${intern.school}</p>
+        </div>
+    </div>
+    `;
+};
+
+const createHTML = (teamCards) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
+        <title>Team Profile Generator</title>
+    </head>
+
+    <body>
+        <!-- title/hero section -->
+        <header class="hero">
+            <h1 class="display-3">My Team</h1>
+        </header>
+
+        <!-- team section -->
+        <div class="container">
+            ${teamCards}
+        </div>
+    </body>
+    </html>
+    `;
+};
 
 module.exports = generateHTML;
